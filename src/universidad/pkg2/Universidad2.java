c858ec8331b2ced51af43718f1620bc1e47c7cda
Marilena Escudero <marusofok2@gmail.com>
2023-10-21 14:35:25 -0300

    crean y arreglan clases debido a error

--- src/universidad/pkg2/Universidad2.java
+++ src/universidad/pkg2/Universidad2.java
@@ -1,21 +1,107 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package universidad.pkg2;
 
-/**
- *
- * @author Usuario
- */
+import java.time.LocalDate;
+import universidad.pkg2.accesoDatos.AlumnoData;
+import universidad.pkg2.accesoDatos.MateriaData;
+import universidad.pkg2.entidades.Alumno;
+import universidad.pkg2.entidades.Materia;
+
 public class Universidad2 {
 
-    /**
-     * @param args the command line arguments
-     */
+   
     public static void main(String[] args) {
-        // TODO code application logic here
+         
+        //trabajamos con la clase alumno
+        
+//      Alumno Angel = new Alumno(8,238745679,"Quiroga","Miguel", LocalDate.of(1990,1,18),true);
+//       Alumno Marce = new Alumno(10, 3748553, "Gonzalez", "Maricel", LocalDate.of(1990, 3, 12), true);
+//       AlumnoData alumn = new AlumnoData();
+//       alumn.guardarAlumno(Marce);
+//       alumn.modificarAlumno(Marce);       
+//       alumn.eliminarAlumno(Marce);
+
+//
+//       Alumno alumnoEncontrado = alumn.buscarAlumno(9);
+//        if (alumnoEncontrado != null) {
+//            System.out.println("dni : " + alumnoEncontrado.getDni());
+//            System.out.println("apelido :" + alumnoEncontrado.getApellido());
+//            System.out.println("nombre  :" + alumnoEncontrado.getNombre());
+//            System.out.println("fecha de nacimiento : " + alumnoEncontrado.getFechaNac());
+//        }
+
+
+//        Alumno alumnoEncontrado = alumn.buscarAlumnoDni(1234);
+//        if (alumnoEncontrado != null) {
+//            System.out.println("dni : " + alumnoEncontrado.getDni());
+//            System.out.println("apelido :" + alumnoEncontrado.getApellido());
+//            System.out.println("nombre  :" + alumnoEncontrado.getNombre());
+//            System.out.println("fecha de nacimiento : " + alumnoEncontrado.getFechaNac());
+//        }
+        
+      
+//        AlumnoData alumn = new AlumnoData();
+//        for (Alumno alumno :alumn.listarAlumnos()) {
+//            System.out.println(" Id de alumno : " + alumno.getIdAlumno());
+//            System.out.println(" DNI alumno : " + alumno.getDni());
+//            System.out.println(" Apellido del alumno : " + alumno.getApellido());
+//            System.out.println(" Nombre del alumno  : " + alumno.getNombre());
+//            System.out.println(" Fecha de nacimiento del alumno : " + alumno.getFechaNac());
+//            System.out.println("-------------------------------------------------------------------");
+//        }
+
+
+//        AlumnoData alumn = new AlumnoData();
+//        for (Alumno alumno : alumn.listarAlumnosBaja()) {
+//            System.out.println(" Id de alumno : " + alumno.getIdAlumno());
+//            System.out.println(" DNI alumno : " + alumno.getDni());
+//            System.out.println(" Apellido del alumno : " + alumno.getApellido());
+//            System.out.println(" Nombre del alumno  : " + alumno.getNombre());
+//            System.out.println(" Fecha de nacimiento del alumno : " + alumno.getFechaNac());
+//            System.out.println("-------------------------------------------------------------------");
+//        }
+
+//trabajamos con la clase materia
+
+//            Materia mat = new Materia(10 ,"Redes", 3,true);
+//            MateriaData nueva = new MateriaData(); // me mofifica a nombre de la materia
+//            nueva.agregarMateria(mat);
+//            System.out.println("Se agrego la materia : " + mat.getNombre());
+//            System.out.println("Se agrego la materia : " + mat.getAnioMateria());
+//            System.out.println("Se agrego la materia : " + mat.getIdMateria());
+//            nueva.modificarMateria(10, 2);
+//            System.out.println("  Id Materia modificada : "  + mat.getIdMateria());
+//            System.out.println(" Materia modificada : "  + mat.getNombre());
+//            System.out.println(" Año modificado : "  + mat.getAnioMateria());
+//
+//            nueva.eliminarMateria(1);
+
+//
+//        MateriaData mat = new MateriaData();
+//        Materia materiaEncontrada = mat.buscarMateria(10);
+//        System.out.println(" Nombre : " + materiaEncontrada.getNombre());
+//        System.out.println(" Año : " + materiaEncontrada.getAnioMateria());
+
+
+
+//            MateriaData mat = new MateriaData();
+//        for (Materia materia :mat.listarMateriasActivas()) {
+//            System.out.println(" Id de materia : " + materia.getIdMateria());
+//            System.out.println(" Nombre de la Materia : " + materia.getNombre());
+//            System.out.println("Año de la materia : " + materia.getAnioMateria());
+//            System.out.println("-------------------------------------------------------------------");
+//        }
+
+
+           MateriaData mat = new MateriaData();
+        for (Materia materia :mat.listarMateriasBajas()) {
+            System.out.println(" Id de materia : " + materia.getIdMateria());
+            System.out.println(" Nombre de la Materia : " + materia.getNombre());
+            System.out.println("Año de la materia : " + materia.getAnioMateria());
+            System.out.println("-------------------------------------------------------------------");
+        }
+        
+       
     }
     
 }
